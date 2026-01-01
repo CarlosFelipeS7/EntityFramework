27c30941857b0ed24648c71315c41480ea32f37c
Carlos Felipe Santos <117771889+CarlosFelipeS7@users.noreply.github.com>
2026-01-01 16:24:59 -0300

    Where

--- FuscaFilmes/FuscaFilmes/DbContexts/Context.cs
+++ FuscaFilmes/FuscaFilmes/DbContexts/Context.cs
@@ -13,6 +13,36 @@ namespace FuscaFilmes.DbContexts
         public DbSet<Filme> Filmes { get; set; } // colecao de objetos do tipo filme
         public DbSet<Diretor> Diretores { get; set; } // colecao de objetos do tipo diretor
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //serve para configurar o modelo de dados, simplificando a criação do banco de dados
+        {
+            modelBuilder.Entity<Diretor>()
+                .HasMany(e => e.Filmes) // Um filme tem um diretor
+                .WithOne(e => e.Diretor) // Um diretor pode ter muitos filmes
+                .HasForeignKey(e => e.DiretorId); // Chave estrangeira no filme
+
+
+            //Insert de dados iniciais
+            modelBuilder.Entity<Diretor>().HasData(
+                new Diretor { Id = 1, Name = "Steven Spielberg" },
+                new Diretor { Id = 2, Name = "Christopher Nolan" },
+                new Diretor { Id = 3, Name = "Quentin Tarantino" },
+                new Diretor { Id = 4, Name = "James Cameron" }
+            );
+
+            modelBuilder.Entity<Filme>().HasData(
+                new Filme { Id = 1, Titulo = "TinTin", Ano = 2010, DiretorId = 1 },
+                new Filme { Id = 2, Titulo = "Jurassic Park", Ano = 1993, DiretorId = 1 },
+                new Filme { Id = 3, Titulo = "Inception", Ano = 2010, DiretorId = 2 },
+                new Filme { Id = 4, Titulo = "Interstellar", Ano = 2014, DiretorId = 2 },
+                new Filme { Id = 5, Titulo = "Pulp Fiction", Ano = 1994, DiretorId = 3 },
+                new Filme { Id = 6, Titulo = "Kill Bill", Ano = 2003, DiretorId = 3 },
+                new Filme { Id = 7, Titulo = "Avatar", Ano = 2009, DiretorId = 4 },
+                new Filme { Id = 8, Titulo = "Titanic", Ano = 1997, DiretorId = 4 }
+            );
+
+
+
+        }
+
     }
 }
--- FuscaFilmes/FuscaFilmes/Entities/Filme.cs
+++ FuscaFilmes/FuscaFilmes/Entities/Filme.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Titulo { get; set; }
         public int Ano { get; set; }
+
+        public int DiretorId { get; set; } // Chave estrangeira para o diretor
         public Diretor Diretor { get; set; }
     }
 }
